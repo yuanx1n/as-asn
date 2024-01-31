ed9b0e4d0869864bc17d4bd66322874a80ac8b14
yuanx1n <liyuanxin682@gmail.com>
2024-02-01 06:24:52 +0800

    •	Minimum and Maximum password age (cannot change password within x mins from the last change of password and must change password after x mins)

--- WebApplication1/Pages/changepass.cshtml.cs
+++ WebApplication1/Pages/changepass.cshtml.cs
@@ -52,29 +52,62 @@ namespace WebApplication1.Pages
                     ModelState.AddModelError(nameof(ChangePasswordModel.newPass), "New password must be different from the current password.");
                     return Page();
                 }
+				// Check if the password change meets the minimum and maximum age requirements
+				var minPasswordAge = TimeSpan.FromMinutes(1); // Set your desired minimum password age
 
-                // Change password
-                var changePasswordResult = await userManager.ChangePasswordAsync(user, ChangePasswordModel.currPass, ChangePasswordModel.newPass);
+				if (user.LastPasswordChange != null)
+				{
+					var timeSinceLastChange = DateTime.UtcNow - user.LastPasswordChange;
 
-                if (changePasswordResult.Succeeded)
-                {
-                    // Password change successful, sign in with the new password
-                    await signInManager.SignInAsync(user, isPersistent: false);
+					if (timeSinceLastChange < minPasswordAge)
+					{
+						ModelState.AddModelError("", $"You cannot change your password within {minPasswordAge.TotalMinutes} minutes of the last change.");
+						return Page();
+					}
 
-                    return RedirectToPage("/Index"); // Redirect to the home page or any other page after a successful password change
-                }
-                else
-                {
-                    // Password change failed, add errors to ModelState
-                    foreach (var error in changePasswordResult.Errors)
-                    {
-                        ModelState.AddModelError("", error.Description);
-                    }
-                }
-            }
+					
+				}
 
-            // If execution reaches here, there are model state errors
-            return Page();
+				// Change password
+				var changePasswordResult = await userManager.ChangePasswordAsync(user, ChangePasswordModel.currPass, ChangePasswordModel.newPass);
+
+				if (changePasswordResult.Succeeded)
+				{
+					// Update LastPasswordChange to the current date and time
+					user.LastPasswordChange = DateTime.UtcNow;
+
+					// Save the updated user information
+					var updateResult = await userManager.UpdateAsync(user);
+
+					if (updateResult.Succeeded)
+					{
+						// Password change successful, sign in with the new password
+						await signInManager.SignInAsync(user, isPersistent: false);
+
+						return RedirectToPage("/Index"); // Redirect to the home page or any other page after a successful password change
+					}
+					else
+					{
+						// Update failed, add errors to ModelState
+						foreach (var error in updateResult.Errors)
+						{
+							ModelState.AddModelError("", error.Description);
+						}
+					}
+				}
+				else
+				{
+					// Password change failed, add errors to ModelState
+					foreach (var error in changePasswordResult.Errors)
+					{
+						ModelState.AddModelError("", error.Description);
+					}
+				}
+
+			}
+
+			// If execution reaches here, there are model state errors
+			return Page();
         }
 
     }
